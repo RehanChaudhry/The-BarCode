09ca816b2e67cb6d34ae798d9d1bb22cc59bdf49
Muhammad Zeeshan <mzeeshan@cygnismedia.com>
2018-09-10 12:30:11 +0500

    Initial Commit

---
+++ TheBarCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheBarCode
+//
+//  Created by Mac OS X on 10/09/2018.
+//  Copyright © 2018 Cygnis Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
