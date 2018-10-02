1d55e477b66eb4875e46f2346434f5aaff5e4de6
Aasna Islam <aasna@cygnismedia.com>
2018-10-02 16:34:39 +0500

    popups ; five a day screen design

---
+++ TheBarCode/ViewControllers/Popups/OutOfCreditViewController.swift
@@ -0,0 +1,44 @@
+//
+//  OutOfCreditViewController.swift
+//  TheBarCode
+//
+//  Created by Aasna Islam on 02/10/2018.
+//  Copyright © 2018 Cygnis Media. All rights reserved.
+//
+
+import UIKit
+
+class OutOfCreditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: IBActions
+    @IBAction func closeButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func reloadButtonTapped(_ sender: Any) {
+    }
+    @IBAction func inviteButtonTapped(_ sender: Any) {
+    }
+    
+}
---
+++ TheBarCode/ViewControllers/Popups/RedeemStartViewController.swift
@@ -0,0 +1,41 @@
+//
+//  RedeemStartViewController.swift
+//  TheBarCode
+//
+//  Created by Aasna Islam on 02/10/2018.
+//  Copyright © 2018 Cygnis Media. All rights reserved.
+//
+
+import UIKit
+
+class RedeemStartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func closeButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func barTenderReadyButtonTapped(_ sender: Any) {
+    }
+    @IBAction func takeMeBackButtonTapped(_ sender: Any) {
+    }
+}
